af4d24e18c662032b28a351f4a9fb68219371ff9
Mahboobraza01 <146864499+Mahboobraza01@users.noreply.github.com>
2025-11-12 19:12:02 +0530

    Update database_connect.php

--- include/database_connect.php
+++ include/database_connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("sql201.epizy.com", "ifo_40389580_pglife_user", "Mahboob@1124", "ifo_40389580_pglife_db");
+$conn = mysqli_connect("127.0.0.1", "root", "", "pg_life");
 
 if (mysqli_connect_errno()) {
     // Throw error message based on ajax or not
